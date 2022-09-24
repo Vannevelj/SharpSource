a7fccffe1294000eacce13a9dc891657a48877b5
Jeroen Vannevel <jer_vannevel@outlook.com>
2022-09-24 18:15:15 +0100

    docs

--- SharpSource/SharpSource/Diagnostics/ComparingStringsWithoutStringComparisonAnalyzer.cs
+++ SharpSource/SharpSource/Diagnostics/ComparingStringsWithoutStringComparisonAnalyzer.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Immutable;
-using System.Xml.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
