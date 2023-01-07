bc8bcd0d97fb7c239066ada7d32178bcc3565e8f
Jeroen Vannevel <jer_vannevel@outlook.com>
2023-01-07 14:40:40 +0000

    using net60 references

--- SharpSource/SharpSource.Test/Helpers/CSCodeFix+Test.cs
+++ SharpSource/SharpSource.Test/Helpers/CSCodeFix+Test.cs
@@ -31,18 +31,11 @@ namespace SharpSource.Test
                     return solution;
                 });
 
-                // After netframework the runtime is split in System.Runtime and System.Private.CoreLib
-                // All references appear to return System.Private.CoreLib so we'll have to manually insert the System.Runtime one
-                TestState.AdditionalReferences.Add(SystemPrivateCoreLibPath);
-                TestState.AdditionalReferences.Add(GetDllDirectory("System.Runtime.dll"));
                 TestState.AdditionalReferences.Add(typeof(Microsoft.AspNetCore.Mvc.FromBodyAttribute).Assembly.Location);
-                TestState.AdditionalReferences.Add(typeof(System.Net.Http.HttpClient).Assembly.Location);
                 TestState.AdditionalReferences.Add(typeof(IHttpClientFactory).Assembly.Location);
-                TestState.AdditionalReferences.Add(typeof(Console).Assembly.Location);
-                TestState.AdditionalReferences.Add(typeof(Enumerable).Assembly.Location);
-                
+
                 // Initialized with an empty object so the underlying test framework doesn't auto-inject all the netcoreapp3.1 references
-                TestState.ReferenceAssemblies = new ReferenceAssemblies("net7.0");
+                TestState.ReferenceAssemblies = ReferenceAssemblies.Net.Net60;
 
 
                 TestState.OutputKind = OutputKind.WindowsApplication;
@@ -54,8 +47,6 @@ end_of_line = lf
 
             protected override bool IsCompilerDiagnosticIncluded(Diagnostic diagnostic, CompilerDiagnostics compilerDiagnostics)
                 => diagnostic.Id is not "CS5001" && base.IsCompilerDiagnosticIncluded(diagnostic, compilerDiagnostics);
-
-            private static string GetDllDirectory(string dllName) => Path.Combine(Path.GetDirectoryName(SystemPrivateCoreLibPath) ?? "", dllName);
         }
     }
 }
